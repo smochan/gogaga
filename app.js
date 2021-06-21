218d6e1c2a9321da61a4bef5fb614738759d468c
smochan <img_2019052@iiitm.ac.in>
2021-06-21 15:49:30 +0530

    some change to port

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ const start = async () => {
     
     app.post("/", add);
     
-    app.listen(3000 || process.env.PORT, function(){
-        console.log("Server started on port 3000");
+    const port = process.env.PORT || 3000;
+    app.listen(port, function(){
+        console.log(`Server started on port ${port}`);
     })
 }
 
