7b7223ab199676dbb8875bdb27809ffb630773f3
smochan <img_2019052@iiitm.ac.in>
2021-06-21 15:41:55 +0530

    changed port for deploying

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const start = async () => {
     
     app.post("/", add);
     
-    app.listen(3000, function(){
+    app.listen(3000 || process.env.PORT, function(){
         console.log("Server started on port 3000");
     })
 }
